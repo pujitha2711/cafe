7fa31e14736fbb04cc8c62c3455a54ec9f96ca16
ManoshReddy01 <gandramanoshreddy@gmail.com>
2026-06-28 17:34:02 +0530

    Configure frontend for production

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import axios from "axios";
+
+axios.defaults.baseURL = "https://cafe-vuqd.onrender.com";
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
